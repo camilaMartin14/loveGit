b009006c363faba89a9cdb6771db448209c55d6e
Camila Martín <camilamartin1403@gmail.com>
2024-06-01 22:54:12 -0300

    creacion metodo saludar

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -4,6 +4,11 @@ public class JavaApplication1 {
 
     public static void main(String[] args) {
 
-        System.out.println("Hola mundo :)");
+        saludar();
+    }
+    
+    public static void saludar(){
+        
+       System.out.println("Hola mundo :)");
     }
 }
